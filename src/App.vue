c8160425d7906ab373ae5df4677b02305a88257e
William <lisirui086@gmail.com>
2023-04-26 23:46:22 +0800

    重构项目

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class='container'></div>
+</template>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+.container {
+  height: 100vh;
+  width: 100vw;
+  background-color: #f0f0f0;
+}
+</style>
